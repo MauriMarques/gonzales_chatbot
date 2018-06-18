2bf488ed9da2b5255cfe26a805bad8f234aa5f2d
mauricio_mmarques <mauricio.mmarques1@gmail.com>
2018-06-18 19:52:28 -0300

    Add support to audio message

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import random
+import os
+import requests
+from requests_toolbelt import MultipartEncoder
 from flask import Flask, request
 from pymessenger.bot import Bot
 from googletrans import Translator
+from gtts import gTTS
 
 
 app = Flask(__name__)
@@ -9,7 +13,7 @@ app = Flask(__name__)
 ACCESS_TOKEN = "EAAIj1rbKwNABALab50gBoXT2eMd1MYhFP0jw6MAG4eLVGwTKYufUxkZAAzllNGxo1Gu59xwloY8IEdJGvoaUOOXEkuJcLUbx4irhbZCvbyxEqss7m3vA5XkwhlxH8ZBQdvayq3IBw5vANx9mE6ec82BZBHHzH3z4qTaFJWQ75Yf0Lb9LX3sM"
 VERIFY_TOKEN = "TOKENDOCHATBOT"
 bot = Bot(ACCESS_TOKEN)
-
+audio_path = "audio.mp3"
 
 @app.route('/', methods=['GET', 'POST'])
 
@@ -27,7 +31,7 @@ def receive_message():
                     recipient_id = message["sender"]["id"]
                     if message["message"].get("text"):
                         response_sent_text = get_translated_message(message["message"].get("text"))
-                        send_message(recipient_id, response_sent_text)
+                        send_message(recipient_id, response_sent_text, audio_path)
 
                     if message["message"].get("attachments"):
                         response_sent_nontext = get_message()
@@ -45,6 +49,10 @@ def verify_fb_token(token_sent):
 def get_translated_message(message_to_translate):
     translator = Translator()
     translation = translator.translate(message_to_translate, src="pt", dest="en")
+
+    tts = gTTS(translation.text)
+    tts.save(audio_path)
+
     return translation.text
 
 
@@ -55,7 +63,30 @@ def get_message():
 
 def send_message(recipient_id, response):
     bot.send_text_message(recipient_id, response)
+    send_audio(recipient_id, audio=audio_path)
     return "success"
+
+
+def send_audio(recipient_id):
+    payload = dict()
+    payload['recipient'] = str({
+        'id': recipient_id
+    })
+    payload['notification_type'] = "REGULAR"
+    payload['message'] = str({
+        'attachment': {
+            'type': "audio",
+            'payload': {}
+        }
+    })
+    payload['filedata'] = (os.path.basename(audio_path), open(audio_path, 'rb'), "audio/mp3")
+
+    multipart_data = MultipartEncoder(payload)
+    multipart_header = {
+        'Content-Type': multipart_data.content_type
+    }
+    return requests.post('{0}/me/messages'.format(bot.graph_url), data=multipart_data,
+                         params=bot.auth_args, headers=multipart_header).json()
 
 
 if __name__ == "__main__":
